9e045b06d6490d5409481d6a8f4d9d611f675e74
Anson <anson@bart.com.hk>
2020-04-07 11:20:22 +0800

    refs#5725

--- AutoDragSortableView.js
+++ AutoDragSortableView.js
@@ -700,8 +700,8 @@ const styles = StyleSheet.create({
     swipe: {
         flexWrap: 'wrap',
         flexDirection: 'row',
-        paddingLeft: 16.5,
-        paddingRight: 16.5
+        marginLeft: 16.5,
+        marginRight: 16.5
     },
     item: {
         position: 'absolute',
